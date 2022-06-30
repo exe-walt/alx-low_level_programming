bd40aedfa526568d5df63e57613d6d1007bd63ac
Paul_Adansi <paul.adansi25@gmail.com>
2022-06-29 21:08:36 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry point  
- * Description: 'the program's description'
- * Return: 0 number
- */
+* main - Entry point  
+* Description: 'the program's description'
+* Return: 0 number
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
