6a0237903d6339bfef7b8ce446246d7f02660014
Paul_Adansi <paul.adansi25@gmail.com>
2022-06-29 21:24:27 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - main function
+* Description: 'the program's description'
+* Return: end program
+*/
+int main(void)
+{
+char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+fprintf(stderr, "%s\n", n);
+return (1);
+}
