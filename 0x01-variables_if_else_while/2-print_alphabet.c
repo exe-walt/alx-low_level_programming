64761cabcbd3fa6aaa7c06dd4d0906c1e25af329
Paul_Adansi <paul.adansi25@gmail.com>
2022-07-01 15:44:34 +0100

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char q = 'a';
+
+while (q <= 'z')
+{
+putchar(q);
+q++;
+}
+
+putchar('\n');
+return (0);
+}
