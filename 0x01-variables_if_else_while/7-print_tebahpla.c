560e1a4b0b5db57b6fac08a5c6bb84b8aae173bf
Paul_Adansi <paul.adansi25@gmail.com>
2022-07-01 16:42:42 +0100

    Betty why?

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints reversed alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i = 'z';
+
+while (i >= 'a')
+{
+putchar(i);
+i--;
+}
+putchar('\n');
+return (0);
+}
