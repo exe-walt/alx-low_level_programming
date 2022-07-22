f489eae2e403a645bb32f86eebb49d0f2573e437
Paul_Adansi <paul.adansi25@gmail.com>
2022-07-22 15:57:57 +0100

    Task 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,53 +2,36 @@
 #include <stdlib.h>
 
 /**
- * main - prints the function.
- * @argc: counts the arguments of the function.
- * @argv: gives the value of the arguments passed to the function.
- *
- * Return: Return always success.
- **/
-
+ * main -> this is a function to print its name
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
 int main(int argc, char *argv[])
 {
-int cents, coins = 0;
+int num, j, result = 0;
+int coins[] = {25, 10, 5, 2, 1};
 
-if (argc == 2)
+if (argc != 2)
 {
-cents = atoi(*(argv + 1));
-while (cents > 0)
-{
-if (cents % 25 < cents)
-{
-cents -= 25;
-coins++;
-}
-else if (cents % 10 < cents)
-{
-cents -= 10;
-coins++;
-}
-else if (cents % 5 < cents)
-{
-cents -= 5;
-coins++;
-}
-else if (cents % 2 < cents)
-{
-cents -= 2;
-coins++;}
-else if (cents % 1 < cents)
-{
-cents -= 1;
-coins++;
-}
-}
-}
-else
-{
-printf("Error\n");
+ printf("%s\n", "Error");
 return (1);
 }
-printf("%d\n", coins);
+
+num = atoi(argv[1]);
+if (num < 0)
+{
+printf("0\n");
+return (0);
+}
+for (j = 0; j < 5 && num >= 0; j++)
+{
+while (num >= coins[j])
+{
+num -= coins[j];
+result++;
+}
+}
+printf("%d\n", result);
 return (0);
 }
