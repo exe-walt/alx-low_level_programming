0e27f26baaecbfda7d4a63b39e0c5ddbf6c8d719
Paul_Adansi <paul.adansi25@gmail.com>
2022-08-15 09:23:20 +0100

    Header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
 #ifndef _LISTS_
 #define _LISTS_
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -17,6 +16,7 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
 
 /**
  * struct listp_s - singly linked list
